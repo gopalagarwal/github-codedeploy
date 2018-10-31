e0edacf04d2c337e304e08bbc557602944fc266a
gopalagarwal <gopal.agarwal@ad.loginradius.net>
2018-10-31 12:30:21 +0530

    change content

--- files/website/node.js
+++ files/website/node.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Helllooooo everybody!');
+res.end('Helllooooo everybody 2!');
 });
 server.listen(80);
